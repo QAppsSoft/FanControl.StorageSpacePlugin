c20d6c52245876d017b95736e95b7dcfdaa8f0e0
alex6dj <alexeiagueroalba@gmail.com>
2022-04-17 11:36:04 -0400

    Catch common exceptions during config file reading

--- src/FanControl.StorageSpacePlugin/Config.cs
+++ src/FanControl.StorageSpacePlugin/Config.cs
@@ -1,10 +1,13 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.IO;
 
 namespace FanControl.StorageSpacePlugin
 {
     internal static class Config
     {
+        private static int _defaultRefreshRate = 30;
+
         public static string ApplicationPath()
         {
             var path = Assembly.GetEntryAssembly().Location;
@@ -23,13 +26,32 @@ namespace FanControl.StorageSpacePlugin
 
         public static int RefreshRate()
         {
-            var configText = File.ReadAllText(ConfigPath());
+            string configText;
+
+            try
+            {
+                configText = File.ReadAllText(ConfigPath());
+            }
+            catch (Exception exception)
+            {
+                switch (exception)
+                {
+                    case FileNotFoundException _:
+                    case DirectoryNotFoundException _:
+                    case PathTooLongException _:
+                    case IOException _:
+                    case UnauthorizedAccessException _:
+                        return _defaultRefreshRate;
+                    default:
+                        throw; // Let FanControl handle and log the Exception
+                }
+            }
 
             var configsArray = configText.Split('=');
 
             var result = int.TryParse(configsArray[1], out var refreshRate);
 
-            return result ? refreshRate : 30;
+            return result ? refreshRate : _defaultRefreshRate;
         }
     }
 }
